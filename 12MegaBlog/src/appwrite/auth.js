a52b7b60887b6eac680e2cb500f89127d000754d
Abhinav <abhinavshaji16@gmail.com>
2026-03-27 12:07:56 +0530

    set env variables created auth service

---
+++ 12MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,67 @@
+import conf from "../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+  client = new Client();
+
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create({
+        userId: ID.unique(),
+        email,
+        password,
+        name,
+      });
+      if (userAccount) {
+        this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      const session = await this.account.createEmailPasswordSession({
+        email,
+        password,
+      });
+      return session;
+    } catch (error) {
+      console.log("Error logging in:", error);
+      throw error;
+    }
+  }
+
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (error) {
+      console.log("Error fetching current user:", error);
+      throw error;
+    }
+    return null;
+  }
+
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      console.log("Error logging out:", error);
+      throw error;
+    }
+  }
+}
+
+const authService = new AuthService();
+
+export default authService;
