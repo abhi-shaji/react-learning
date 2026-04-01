0f7b6979055e0a2869e2592b3f9c6eb0f803d912
Abhinav <abhinavshaji16@gmail.com>
2026-04-01 10:52:53 +0530

    rte and postform components

--- 12MegaBlog/src/components/index.js
+++ 12MegaBlog/src/components/index.js
@@ -1,9 +1,21 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./container/Container"
-import Logo from './Logo' 
-import LogoutBtn from './Header/LogoutBtn'
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
 import Login from "./Login";
 import Button from "./Button";
+import Select from "./Select";
+import RTE from "./RTE";
 
-export {Header, Footer, Container, Logo, LogoutBtn, Login, Button} 
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Login,
+  Button,
+  Select,
+  RTE,
+};
